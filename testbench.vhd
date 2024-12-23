cb6c1e8a35b4368500c77e81db36f9f3c79d7062
LucasEmmanuelRM <146481992+LucasEmmanuelRM@users.noreply.github.com>
2024-12-23 15:12:17 -0300

    Add files via upload

---
+++ testbench.vhd
@@ -0,0 +1,38 @@
+-- Code your testbench here
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity testbench is
+end entity testbench;
+
+architecture behavior of testbench is
+	signal clock, reset		: std_logic := '0';
+    signal state			: std_logic_vector := "0000";
+    constant clock_period	: time := 10 ns;
+    
+begin
+	myRisc: entity work.design port map (
+    	clk => clock,
+        rst => reset,
+        state => state
+        );
+        
+	clockGenerator : process
+    begin
+    	for i in 0 to 25 loop
+        	clock <= '0';
+            wait for clock_period/2;
+            clock <= '1';
+            wait for clock_period/2;
+        end loop;
+        wait;
+    end process;
+    
+    process
+    begin
+    	reset <= '1';
+        wait for 10 ns;
+        reset <= '0';
+    wait;
+    end process;
+end behavior;
